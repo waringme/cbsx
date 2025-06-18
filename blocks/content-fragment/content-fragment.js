1aec832e4b1e81388eed8f6ffc76eaf423a6c958
Tim Waring <waring@adobe.com>
2025-06-18 10:58:10 +0100

    sss

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -6,60 +6,27 @@ function getMetadata(name) {
   return meta || '';
 }
 
-//const aem = "http://localhost:4503";
-//const aem = "https://publish-p107058-e1001010.adobeaemcloud.com";
-const aem = "https://publish-p150634-e1553296.adobeaemcloud.com";
-const cors = "https://cors.cpilsworth.workers.dev/?target=";
+adobe.target.getOffer({
+  "mbox": "tim-mbox",
+  "params": {},
+  "success": function(offer) {
+        console.log(offer[0].content[0].greeting);
+         },
+  "error": function(status, error) {
+      console.log('Error', status, error);
+  }
+});
+
+
 
 export default function decorate(block) {
-
-  const slugID = document.createElement('div');
-  slugID.id = 'slug';
-  slugID.textContent = block.querySelector('div:nth-child(1)').textContent.trim();
-//  block.querySelector('div:nth-child(1)').replaceWith(slugID);
-
   const destinationDiv = document.createElement('div');
-  destinationDiv.id = `destination-${slugID.textContent}`;
-  block.querySelector('div:last-of-type').replaceWith(destinationDiv);
-  console.log("Slug ID");  
-console.log(slugID.textContent);
-console.log(Date.now()); 
-const urlEndpoint = cors + aem + "/graphql/execute.json/bq/getPageBySlugAndVariation;slug=" + slugID.textContent + "&ck=" + Date.now();
-//const urlEndpoint = cors + aem + "/graphql/execute.json/bq/getPageBySlugAndVariation;slug=1?ck=34356";
-console.log(urlEndpoint);
-
-// fetch('https://cors.cpilsworth.workers.dev/?target=https://publish-p150634-e1553296.adobeaemcloud.com/graphql/execute.json/nationwide/mortgage-offer-by-slug;slug=offer-1')
-
- fetch(urlEndpoint)
-    .then(response => response.json())
-    .then(response => {
-      const {  image, type, description, furtherInformation} = response.data.lawnmowerInformationList.items[0];
-      const imageURL = `${aem}${image._dynamicUrl}`;
-
-      console.log(type);
-
-   
-
-console.log("description");
-console.log(description.html);
-//console.log("furtherInformation");
-//console.log(furtherInformation.html);
-
-      destinationDiv.innerHTML = `
+  destinationDiv.innerHTML = `
         <div class="textCommon section">
-          <div class="destination-content-type"><h3>${type}</h3></div>
-          <div class="destination-content-wrapper">
-            <div class="destination-image">
-              <img src="${imageURL}" alt="Lawnmower">
-            </div>
-            <div class="destination-content-description">${description.html}</div>
-          </div>
-          <div class="destination-content-further-information">${furtherInformation.html}</div>
-        </div>
+          <div class="destination-content-type"><h3>Hello</h3></div>
+         </div>
+         </div>
       `;
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
+
 
 }
