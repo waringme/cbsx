63c42125aa1b7f85b935587ac7ffaa58cb9e3665
Tim Waring <waring@adobe.com>
2025-06-18 10:38:56 +0100

    added content fragment

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -148,7 +148,7 @@ async function getAndApplyOffers() {
 }
 
 let atjsPromise = Promise.resolve();
-console.log('Title metadata found: ' + getMetadata('Title'));
+console.log('Title metadata found: ' + getMetadata('itle'));
 console.log('URL metadata found: ' + getMetadata('url'));
 if (getMetadata('target')) {
   console.log('Target metadata found: ' + getMetadata('target'));
