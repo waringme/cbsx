ecb66aa9d01a923bd6acec589133db4d52954bc8
Tim Waring <waring@adobe.com>
2025-06-18 12:25:43 +0100

    calls target for the cbsx activity

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -6,27 +6,51 @@ function getMetadata(name) {
   return meta || '';
 }
 
-adobe.target.getOffer({
-  "mbox": "tim-mbox",
-  "params": {},
-  "success": function(offer) {
-        console.log(offer[0].content[0].greeting);
-         },
-  "error": function(status, error) {
-      console.log('Error', status, error);
-  }
-});
-
-
-
 export default function decorate(block) {
   const destinationDiv = document.createElement('div');
-  destinationDiv.innerHTML = `
+
+  adobe.target.getOffer({
+    "mbox": "tim-mbox",
+    "params": {},
+    "success": function(offer) {
+      const offerData = offer[0].content[0].data.offerBankByPath.item;
+      console.log(offerData.title);
+      
+      let title = offerData.title;
+      let detail = offerData.detail.plaintext;
+      let image = offerData.image._publishUrl;
+      let cta = offerData.cta;
+      let ctaLink = offerData.ctaLink;
+      
+      // Update the HTML with all the dynamic content
+      destinationDiv.innerHTML = `
         <div class="textCommon section">
-          <div class="destination-content-type"><h3>Hello</h3></div>
-         </div>
-         </div>
+          <div class="destination-content-type">
+            <h3>${title}</h3>
+            <p>${detail}</p>
+            <img src="${image}" alt="${title}" />
+            <a href="${ctaLink}" class="cta-button">${cta}</a>
+          </div>
+        </div>
       `;
+    },
+    "error": function(status, error) {
+      console.log('Error', status, error);
+      // Fallback content if there's an error
+      destinationDiv.innerHTML = `
+        <div class="textCommon section">
+          <div class="destination-content-type"><h3>Welcome</h3></div>
+        </div>
+      `;
+    }
+  });
 
-
+  block.querySelector('div:last-of-type').replaceWith(destinationDiv);
+  
+  // Initial content while waiting for the offer
+  destinationDiv.innerHTML = `
+    <div class="textCommon section">
+      <div class="destination-content-type"><h3>Loading...</h3></div>
+    </div>
+  `;
 }
